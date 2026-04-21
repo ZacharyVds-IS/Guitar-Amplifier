a0e532fcc45bb1e6ad1b9c75095a89f3cf242072
ZacharyVds-IS <zachary.vandestaey@infosupport.com>
2026-04-21 08:45:52 +0200

    chore: removed placeholder logic from backend.

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -1,14 +1,12 @@
 pub mod commands;
 pub mod services;
-
-use crate::commands::greet::greet;
 use crate::commands::loopback::start_loopback;
 
 #[cfg_attr(mobile, tauri::mobile_entry_point)]
 pub fn run() {
     tauri::Builder::default()
         .plugin(tauri_plugin_opener::init())
-        .invoke_handler(tauri::generate_handler![greet, start_loopback])
+        .invoke_handler(tauri::generate_handler![start_loopback])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
